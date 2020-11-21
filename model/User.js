f077f48838b13593d9b55230b2455bcc0011660b
tannv_57 <NVantan94@gmail.com>
2020-11-21 17:31:04 +0900

    add user model

---
+++ model/User.js
@@ -0,0 +1,33 @@
+import mongoose from 'mongoose'
+
+const userSchema = mongoose.Schema({
+  name: {
+    type: String,
+    maxLength: 50
+  },
+  email: {
+    type: String,
+    trim: true,
+    unique: 1
+  },
+  password: {
+    type: String,
+    minLength: 5
+  },
+  lastname: {
+    type: String,
+    maxLength: 50
+  },
+  role: {
+    type: Number,
+    default: 0
+  },
+  token: {
+    type: String
+  },
+  tokenExp: {
+    type: Number
+  }
+})
+
+export default mongoose.model('User', userSchema)
