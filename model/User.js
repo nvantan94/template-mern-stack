bb4204ed7f2ac2b5021a1ee012b4760dbea30a6f
tannv_57 <NVantan94@gmail.com>
2020-11-22 12:05:07 +0900

    encrypt password before saving

--- model/User.js
+++ model/User.js
@@ -1,5 +1,7 @@
 import mongoose from 'mongoose'
+import bcrypt from 'bcrypt'
 
+const saltRounds = 10;
 const userSchema = mongoose.Schema({
   name: {
     type: String,
@@ -27,6 +29,25 @@ const userSchema = mongoose.Schema({
   },
   tokenExp: {
     type: Number
+  }
+});
+
+userSchema.pre('save', function(next) {
+  var user = this;
+
+  if (user.isModified('password')) {
+    bcrypt.genSalt(saltRounds, function(err, salt) {
+      if (err)
+        return next(err);
+      bcrypt.hash(user.password, salt, (err, hash) => {
+        if (err)
+          return next(err);
+        user.password = hash;
+        next();
+      })
+    })
+  } else {
+    next();
   }
 })
 
